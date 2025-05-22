1a491a77ce4f3a912e91f923c957eebca1ebac86
indextrown <indextrown@gmail.com>
2025-05-22 16:04:15 +0900

    feat: 사진 확대 기능 추가

--- Haruhancut/Source/Presentation/Camera/ImagePreview/ImagePreviewController.swift
+++ Haruhancut/Source/Presentation/Camera/ImagePreview/ImagePreviewController.swift
@@ -8,19 +8,19 @@
 import UIKit
 
 // MARK: - 이미지 프리뷰
-final class ImagePreviewViewController: UIViewController {
+final class ImagePreviewViewController: UIViewController, UIScrollViewDelegate {
     
     private let image: UIImage
     
-    init(image: UIImage) {
-        self.image = image
-        super.init(nibName: nil, bundle: nil)
-        modalPresentationStyle = .fullScreen
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    // zoom in/out을 위한 스크롤뷰 추가
+    private lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.minimumZoomScale = 1.0
+        sv.maximumZoomScale = 3.0
+        sv.delegate = self
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
 
     private lazy var imageView: UIImageView = {
         let iv = UIImageView(image: image)
@@ -49,19 +49,41 @@ final class ImagePreviewViewController: UIViewController {
         btn.addTarget(self, action: #selector(savePreview), for: .touchUpInside)
         return btn
     }()
+    
+    init(image: UIImage) {
+        self.image = image
+        super.init(nibName: nil, bundle: nil)
+        modalPresentationStyle = .fullScreen
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
         view.addSubview(closeButton)
         view.addSubview(saveButton)
 
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: view.topAnchor),
-            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            // scrollView가 전체 화면을 덮도록
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            // imageView는 scrollView크기와 동일
+            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            
+            imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            
 
             // 닫기 버튼
             closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
@@ -88,6 +110,11 @@ final class ImagePreviewViewController: UIViewController {
                                       preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .default))
         present(alert, animated: true)
+    }
+    
+    // 줌 대상 지정
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
 
