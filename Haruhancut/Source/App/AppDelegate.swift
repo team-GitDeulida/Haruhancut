0fedbe56eee70af2aee7910938779a937198a618
indextrown <indextrown@gmail.com>
2025-05-23 00:32:02 +0900

    feat: fcm 기능 추가

--- Haruhancut/Source/App/AppDelegate.swift
+++ Haruhancut/Source/App/AppDelegate.swift
@@ -16,6 +16,12 @@ import RxKakaoSDKCommon
 //import RxKakaoSDKAuth
 import KakaoSDKAuth
 
+import Foundation
+
+// 알람 관련
+import FirebaseMessaging
+import UserNotifications
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -24,12 +30,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 파이어베이스 설정
         FirebaseApp.configure()
         
+        // 앱 실행 시 사용자에게 알림 허용 권한 받기
+        UNUserNotificationCenter.current().delegate = self
+        
+        // 파이어베이스 Meesaging 설정
+        Messaging.messaging().delegate = self
+        
+        // 알림 권한 호출
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            print("✅ 알림 권한: \(granted)")
+            guard granted else { return }
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+        
+        
         // 카카오톡 설정
         if let nativeAppKey: String = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] as? String {
             RxKakaoSDK.initSDK(appKey: nativeAppKey, loggingEnable: false)
         }
  
-        // 의존성 주입ㅇ
+        // 의존성 주입
         registerDependencies()
         
         return true
@@ -59,6 +82,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
+
+// MARK: - 알람관련
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // 포그라운드(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+    }
+}
+
 
 /*
  - DIContainer는 프로젝트 전반에서 객체를 주입받을 수 있도록 관리하는 싱글톤이다
